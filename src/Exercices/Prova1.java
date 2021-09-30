108eed43074473413fc2f7824189aeb0a037f2d6
sriverogalan <56429173+sriverogalan@users.noreply.github.com>
2021-09-30 08:48:15 +0200

    Primer Commit

---
+++ src/Exercices/Prova1.java
@@ -0,0 +1,8 @@
+package Exercices;
+
+public class Prova1 {
+    public static void ex1() {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Exercices.Prova1;
+
+public class Main {
+
+    public static void main(String[] args){
+        Prova1.ex1();
+
+    }
+
+}
